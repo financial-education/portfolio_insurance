0970ca092d611a9a22dcd26d737d5aaa35d81b1f
Matt Brigida <mdbrigida@gmail.com>
2019-11-10 20:30:02 -0500

    added gamma calculation

--- src/app1.js
+++ src/app1.js
@@ -30,9 +30,9 @@ function myJsFunction()
     var y1 = parseFloat(document.getElementById('myRange').value);
     var d1 = (Math.log(y1 / 50) + (.01 + (.2 * .2) / 2) * 1) / (.2 * 1);
     var delta =  (1 + erf(d1 / Math.sqrt(2))) / 2 - 1; // need js equiv of delta.p <- function(){pnorm(d1()) - 1}
-    var gamma =   //gamma <- function(){dnorm(d1()) / (stock * vol * sqrt(time))}
+    var gamma =   (1 / Math.sqrt(2 * Math.PI)) * Math.exp(-(d1 * d1) / 2);//gamma <- function(){dnorm(d1()) / (stock * vol * sqrt(time))}
     document.getElementById("out1").innerHTML = delta.toFixed(2);
-//    document.getElementById("out2").innerHTML = CFs.toFixed(2);
+    document.getElementById("out2").innerHTML = gamma.toFixed(2);
     // document.getElementById("out0").innerHTML = y1.toFixed(2);
 
 }
